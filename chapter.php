9e14bf5bc39d1ad9bd4c3e9977a1ae308f8392b9
sora-coder <rqqgx271@yahoo.co.jp>
2020-11-15 11:23:06 +0900

    連想配列とfor文

--- chapter.php
+++ chapter.php
@@ -16,6 +16,17 @@
             'remon'=>'レモン',
             'cherry'
         ];
+        echo "<br/>";
+
+        $x = -5;
+        if($x > 30){
+            echo "30以上です。";
+        } elseif($x <= 30 && $x > 0){
+            echo('0以上30以下です。');
+        } else{
+            echo('マイナスです。');
+        }
+        echo "<br/>";
         foreach($fruits as $name) {
             echo "{$name}が美味しい！"."<br/>";
         }
@@ -28,16 +39,19 @@
         ];
         echo $leader[0]."<br/>";
 
-
-
-        $x = -5;
-        if($x > 30){
-            echo "30以上です。";
-        } elseif($x <= 30 && $x > 0){
-            echo('0以上30以下です。');
-        } else{
-            echo('マイナスです。');
-        }
+        $kyojin = [
+            '清水', '二岡', '高橋', '松井'
+        ];
+        $kyojin2 = [
+            '清原', '江藤', '元木'
+        ];
+        $marged = array_merge($kyojin, $kyojin2);
+        var_dump($marged);
+        echo "<br/>";
+        $cnt = count($marged);
+        for($x = 1; $x < $cnt; $x++){
+                echo "{$x}番　{$marged[$x - 1]}"."<br/>";
+            }
 
 
 
